8b583ffc092681bdf17212bdfb1fc09242859b26
EriottosanMC <94922833+EPats@users.noreply.github.com>
2023-10-17 19:37:34 +0100

    RottenToTheStore

--- src/main/java/io/github/epats/rottentothecore/RottenFamilyTree.java
+++ src/main/java/io/github/epats/rottentothecore/RottenFamilyTree.java
@@ -6,6 +6,8 @@ public class RottenFamilyTree {
 
     public static final String ROTTEN_TO_THE_CORE = "rottentothecore";
     public static final String ROTTEN_NEVERMORE = "rottennevermore";
+    public static final String ROTTEN_NIGHTTIMES_ROAR = "rottennighttimesroar";
+    public static final String ROTTEN_TO_THE_STORE = "rottentothestore";
 
     private static boolean isModLoaded(String modId) {
         return ModList.get().isLoaded(modId);
